0be89aa2687b773e54549c8afef0c603b4670253
FerRory <rory@ferrory.net>
2025-12-02 09:31:48 +0100

    Keep workers at 1

--- talos-cluster/talos-cluster-create-for-podman.sh
+++ talos-cluster/talos-cluster-create-for-podman.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-talosctl cluster create --config-patch @patch.yaml --skip-k8s-node-readiness-check -p 8080:8080/tcp
+talosctl cluster create --config-patch @patch.yaml --workers 1 --skip-k8s-node-readiness-check -p 8080:8080/tcp
 
 kubectl kustomize --enable-helm=true  ../cilium | kubectl apply -f -
 
@@ -8,6 +8,7 @@ cilium status --wait
 
 kubectl kustomize --enable-helm=true ../argocd | kubectl apply -f -
 
-echo "Sleep 15 seconds..."
+echo "Sleep 60 seconds..."
+sleep 60
 
 kubectl apply -f ../argo-apps/root-app.yaml
