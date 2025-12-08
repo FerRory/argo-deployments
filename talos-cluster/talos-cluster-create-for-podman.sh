f4c2ed8b0ac18c6f1ec29370d49c14efcaba4fa5
FerRory <rory@ferrory.net>
2025-12-08 08:56:51 +0100

    Use 3 workers and cleanup charts directory.

--- talos-cluster/talos-cluster-create-for-podman.sh
+++ talos-cluster/talos-cluster-create-for-podman.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-talosctl cluster create --config-patch @patch.yaml --workers 1 --skip-k8s-node-readiness-check -p 8080:8080/tcp
+talosctl cluster create --config-patch @patch.yaml --workers 3 --skip-k8s-node-readiness-check -p 8080:8080/tcp
 
 kubectl kustomize --enable-helm=true  ../cilium | kubectl apply -f -
 
@@ -8,6 +8,7 @@ cilium status --wait
 
 kubectl kustomize --enable-helm=true ../argocd | kubectl apply -f -
 
+rm -rf ../cilium/charts
 echo "Sleep 60 seconds..."
 sleep 60
 
